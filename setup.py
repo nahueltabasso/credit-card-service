209adc946469b1fc7b7689b0c460567e8e5ae50e
nahueltabasso <nahueltabasso@gmail.com>
2024-09-05 09:02:49 -0300

    build: fix setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version="1.0",
     packages=find_packages(),
     author="Nahuel Tabasso",
-    author_email="nahueltabasso@gmail.com",
-    description="This poject...",
-    long_description=open("README.adoc").read(),
-    long_description_content_type="text/markdown",
-    url="url git"
+    # author_email="nahueltabasso@gmail.com",
+    # description="This poject...",
+    # long_description=open("README.adoc").read(),
+    # long_description_content_type="text/markdown",
+    # url="url git"
 )
