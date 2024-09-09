8c95fba37f76f5f85b019d53cc98eb5440671bf4
Nahuel Tabasso <nahueltabasso@gmail.com>
2024-09-08 22:55:46 -0300

    fix: fix extract payment network with coords

--- src/api/main.py
+++ src/api/main.py
@@ -14,68 +14,14 @@ app.include_router(v2_router, prefix="/api/v2/service/credit-card", tags=["v2"])
 
 config = Config()
 
-# Create an instance of OCRService
-ocrService = get_ocr_service()
-# Create an instance of CardService
-card_service = get_card_service()
+# # Create an instance of OCRService
+# ocrService = get_ocr_service()
+# # Create an instance of CardService
+# card_service = get_card_service()
 
 @app.get("/api/health", status_code=200)
 def get_service_status() -> dict:
     return {"status": "Ok"}
 
-# @app.post("/api/v1/service/credit-card", status_code=200)
-# async def credit_card_service(payment_network: str, file: UploadFile = File(...)):
-#     print("Enter to credit_card_service()")
-#     print(f"Image from request --- {file}")
-    
-#     # Valid that file is an image
-#     image = validate_image(file=file)
-#     # Convert file to numpy array
-#     img_np = image_to_numpy(image=image)
-#     # Detect the credit card and payment network
-#     credit_card, payment_network = credit_card_detector(img=img_np)
-#     response = CreditCardData()
-#     if credit_card is not None and payment_network is not None:
-#         response.payment_network = payment_network
-#         ocrService.set_img(img=credit_card)
-#         ocrService.set_zones_coords(zones=get_zones_coords(payment_network))
-#         response = ocrService.extract(entity=response)
-#         response.obs = "Succesfull process!"
-#     else:
-#         response.obs = "Can not detected a credit card."
-
-#     return response
-
-# @app.post("/api/v2/service/credit-card", status_code=200)
-# async def get_data(file: UploadFile = File(...)):
-#     print("Enter to credit_card_service()")
-#     print(f"Image from request --- {file}")
-    
-#     # Valid that file is an image
-#     image = validate_image(file=file)
-#     # Convert file to numpy array
-#     img_np = image_to_numpy(image=image)
-#     # Detect a card in the image
-#     card = card_service.get_card_bbox(input_img=img_np)
-#     response = CreditCardData()
-#     if card is not None:
-#         # Detect the card elements
-#         card_elements = card_service.get_card_elements(card=card)
-#         # Classify the payment network
-#         payment_network = card_service.classify_payment_network(
-#             element=card_elements
-#         )
-#         response.payment_network = payment_network
-#         ocrService.set_img(img=card)
-#         ocrService.set_images(elements=card_elements)
-#         ocrService.set_zones_coords(zones=config.COMMON_CARD_ZONES)
-#         response = ocrService.extract(entity=response)
-#         response.obs = "Succesfull process!"
-#     else:
-#         response.obs = "Invalid image"
-#     return response
-    
-
-        
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/core/card_service.py
+++ src/core/card_service.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 from dotenv import load_dotenv
-from src.utils.file_utils import crop_image, show_image
+from src.utils.file_utils import crop_image, show_image, extract_zone
 from src.config.config import Config
 import numpy as np
 import os
@@ -43,6 +43,21 @@ class CardService:
         return cls._instance
     
     def _inference(self, detector: any, img: np.ndarray, iou_threshold: float=0.5) -> tuple:
+        """
+        Run the inference process on the provided image using the specified detector.
+
+        Args:
+            detector (any): The detection model to be used for inference.
+            img (np.ndarray): The input image on which to run inference.
+            iou_threshold (float, optional): Intersection over Union (IoU) threshold for 
+                                             Non-Max Suppression (NMS). Default is 0.5.
+        
+        Returns:
+            tuple: A tuple containing two lists:
+                - xyxy (list): List of bounding boxes' coordinates after applying NMS.
+                - clss (list): List of class labels corresponding to the selected bounding boxes.
+            
+        """
         result = detector(source=img)
 
         xyxy = result[0].boxes.xyxy.cpu()
@@ -61,8 +76,23 @@ class CardService:
 
     
     def get_card_bbox(self, input_img: np.ndarray, show: bool=False):
-        result = self.card_detector(source=input_img)
-        
+        """
+        Detects the bounding box of a credit or debit card in the input image.
+
+        This function uses a YOLO model to detect a credit/debit card within the provided
+        input image. It validates whether exactly one card is detected, and if so, crops 
+        the card from the image and optionally displays the input and cropped images.
+
+        Args:
+            input_img (np.ndarray): The input image in which to detect the card.
+            show (bool, optional): Flag to indicate whether to display the images.
+                                   If set to True, the function will display both the input
+                                   and cropped card images. Default is False.
+
+        Returns:
+            np.ndarray or None: The cropped image of the credit/debit card if one is found.
+                                Returns None if no card or multiple cards are detected.
+        """
         # Validate if the model detected only one credit/debit card, else the image is not valid
         xyxy, clss = self._inference(detector=self.card_detector,
                                       img=input_img)
@@ -82,22 +112,66 @@ class CardService:
         return None
     
     def get_card_elements(self, card: np.ndarray, show: bool=False) -> dict:
+        """
+        Extracts various elements such as card number, expiry date, cardholder name,
+        and payment network from the provided credit/debit card image.
+
+        This function uses a YOLO model to detect different elements of a credit/debit
+        card within the input card image. It then crops these elements from the card
+        image and organizes them into a dictionary. Optionally, the function can display
+        the cropped elements.
+
+        Args:
+            card (np.ndarray): The input image of the credit/debit card.
+            show (bool, optional): Flag to indicate whether to display the cropped elements.
+                                   If set to True, the function will display each cropped
+                                   element with a corresponding label. Default is False.
+
+        Returns:
+            dict: A dictionary containing the cropped elements of the credit/debit card.
+                  The keys of the dictionary are:
+                  - 'card_number': The image of the card number.
+                  - 'expiry_date': The image of the expiry date.
+                  - 'cardholder': The image of the cardholder name.
+                  - 'payment_network': The image of the payment network logo.
+                  Each key will have a value of None if the corresponding element is not detected.
+        """
         xyxy, clss = self._inference(detector=self.elements_detector,
                                      img=card)
         
         # Cut the card elements from credit card image and set dictionary with values
         elements_dict = self._set_elements(card=card,
-                                      boxes=xyxy,
-                                      clss=clss)
+                                           boxes=xyxy,
+                                           clss=clss)
         if show:
             [show_image(img=value, label=key) for key, value in elements_dict.items()]
         return elements_dict
     
     def classify_payment_network(self, element: np.ndarray, card: np.ndarray) -> str:
+        """
+        Classifies the payment network of a given credit/debit card using a trained YOLO model.
+
+        This function takes an element (typically the cropped payment network logo) from a credit/debit card
+        and classifies it into one of several known payment network categories. If the element is not provided,
+        it uses a predefined bounding box to crop the payment network region from the card image.
+
+        Args:
+            element (np.ndarray): The image of the payment network logo to be classified. If None, the function
+                                  will crop the image using a predefined bounding box from the card image.
+            card (np.ndarray): The image of the credit/debit card from which to crop the payment network logo
+                               if element is not provided.
+
+        Returns:
+            str: The classified payment network. Possible values are constants defined in the Config class:
+                 - config.AMERICAN_EXPRESS_CONSTANT
+                 - config.CABAL_CONSTANT
+                 - config.MASTERCARD_CONSTANT
+                 - config.VISA_CONSTANT
+                 Returns None if the classification is not successful.
+        """
         config = Config()
         if element is None:
-            element = crop_image(img=card,
-                                 bbox=config.COMMON_CARD_ZONES['payment_network'])
+            element = extract_zone(img=card, zone=config.COMMON_CARD_ZONES['payment_network'])
         result = self.classifier(source=element)
         top1 = result[0].probs.top1
         top1_conf = result[0].probs.top1conf
