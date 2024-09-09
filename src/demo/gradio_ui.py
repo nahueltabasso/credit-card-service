d2b7706ccacc676829d6ab4981f3f3ea74ff8214
Nahuel Tabasso <nahueltabasso@gmail.com>
2024-09-08 23:02:42 -0300

    research: gradio demo

--- src/demo/gradio_ui.py
+++ src/demo/gradio_ui.py
@@ -1,28 +1,36 @@
-from src.core.credit_card_processor import credit_card_detector, get_zones_coords
 from src.core.ocr_service import get_ocr_service
+from src.core.card_service import get_card_service
+from src.config.config import Config
 from src.models.model import CreditCardData
 import gradio as gr
 import cv2
 import json
 
-choices = ["VISA", "MASTER CARD", "AMERICAN EXPRESS", "CABAL"]
-
+config = Config()
 ocr_service = get_ocr_service()
+card_service = get_card_service()
 
-def process(image_input, payment_network_input):
+def process(image_input):
     print("Enter to process()")
     
-    credit_card, payment_network = credit_card_detector(img=image_input)
+    card = card_service.get_card_bbox(input_img=image_input)
     response = CreditCardData()
-    if credit_card is not None and payment_network is not None:
+    if card is not None:
+        # Detect the card elements
+        card_elements = card_service.get_card_elements(card=card)
+        # Classify the payment network
+        payment_network = card_service.classify_payment_network(
+            element=card_elements['payment_network'],
+            card=card
+        )
         response.payment_network = payment_network
-        ocr_service.set_img(img=credit_card)
-        ocr_service.set_zones_coords(zones=get_zones_coords(payment_network))
+        ocr_service.set_img(img=card)
+        ocr_service.set_images(elements=card_elements)
+        ocr_service.set_zones_coords(zones=config.COMMON_CARD_ZONES)
         response = ocr_service.extract(entity=response)
-        response.obs = "Succesfull process"
+        response.obs = "Succesfull process!"
     else:
-        response.abs = "Can not detected a credit card"
-
+        response.obs = "Invalid image"
     response_dict = response.to_dict()
     json_response = json.dumps(response_dict, indent=4)
     return json_response
@@ -41,7 +49,6 @@ with gr.Blocks() as demo:
     
     with gr.Row():
         image_input = gr.inputs.Image(type="numpy", label="Credit Card")
-        payment_network_input = gr.Dropdown(choices=choices, label="Cases")
 
     with gr.Row():
         process_button = gr.Button("SUBMIT")
@@ -50,7 +57,7 @@ with gr.Blocks() as demo:
         output_json = gr.JSON()
     
     process_button.click(process, 
-                         inputs=[image_input, payment_network_input],
+                         inputs=[image_input],
                          outputs=[output_json])
         
 demo.launch(share=True)
